a89ea2100b74bc5a1f321234a11eee58a9ebe605
YURI DE OLIVEIRA RODRIGUES <87007240097@senacrs.edu.br>
2026-04-22 21:42:30 -0300

    atualizando as funçoes

--- Main.py
+++ Main.py
@@ -1,36 +1,59 @@
 import time 
-import os 
-from tarefas import adicionar_tarefa, listar_tarefas,carregar_dados,concluir_tarefas
+import os
+
+from tarefas import adicionar_tarefa, listar_tarefas,carregar_dados,concluir_tarefas, limpar_tela
 
 carregar_dados()
+limpar_tela()
 
 TEMPO = 3
 
 while True:
-    print("*****Lista de Tarefas*****")
+    limpar_tela()
+    print("=====GERENCIADOR DE TAREFAS=====")
     print("1- Adicionar Tarefas. ")
+    print("______________________")
     print("2- Listar Tarefas. ")
+    print("______________________")
     print("3-Concluir Tarefas. ")
+    print("______________________")
     print("4- Sair. ")
+    
+    try:
+        opcao = int(input("Escolha uma opção: "))
 
-    opcao = int(input("Escolha uma opção: "))
-   
-    if opcao == 1:
-        descricao = input("Digite uma tarefa: ")
-        adicionar_tarefa(descricao)
-        
-                                    
-    elif opcao == 2:
-        print("\nSuas Tarefas:")
-        listar_tarefas()
+        if opcao == 1:
+            limpar_tela()
+            descricao = input("Digite uma tarefa: ")
+            adicionar_tarefa(descricao)
+            time.sleep(TEMPO)
+
+        elif opcao == 2: 
+            
+            limpar_tela()
+            print("\nSuas Tarefas:")
+            listar_tarefas()
+            time.sleep(TEMPO)
+            
+        elif opcao == 3:
+            limpar_tela()
+            listar_tarefas()
+            tarefa = int(input("Qual tarefa deseja concluir? "))
+            concluir_tarefas(tarefa) 
+
+            time.sleep(TEMPO)
     
-    elif opcao == 3:
-        input = (int("Digite um numero de tarefas! ")) 
-    
-    elif opcao == 4:
-        print("Saindo do programa...")
-       
-        break
-        
-    else:
-        print("Opção inválida, tente novamente.")         
+        elif opcao == 4:
+            limpar_tela()
+            time.sleep(TEMPO)
+            print("Saindo do programa...")
+            break
+            
+        else:
+            limpar_tela()
+            time.sleep(TEMPO)
+            print("Opção inválida, tente novamente.")         
+
+    except ValueError: 
+        print("Erro: Digite apenas numeros")
+        time.sleep(TEMPO)
--- tarefas.py
+++ tarefas.py
@@ -1,16 +1,20 @@
 import json
+import os
 
 lista_tarefas = []
-
+ 
 def adicionar_tarefa(descricao):
     tarefa = {
             "descricao" : descricao,
             "concluida" : False
         } 
-    
     lista_tarefas.append(tarefa)
     print("tarefa adicinada!")
-    
+    salvar_dados()
+
+def limpar_tela(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def listar_tarefas():
     for i, tarefa in enumerate(lista_tarefas, 0):
         if tarefa['concluida']: 
@@ -20,8 +24,13 @@ def listar_tarefas():
 
 def concluir_tarefas(indice):
    try:
-        lista_tarefas[indice]['concluida'] = True
-        salvar_dados()
+        if lista_tarefas[indice]['concluida']:
+            print("Tarefa ja foi Concluida! ")
+        
+        else: 
+            lista_tarefas[indice]['concluida'] = True
+            salvar_dados()
+            print("Tarefa Concluida com sucesso! ")
    except IndexError:
        print("Erro:⚠️ indice invalido! tarefa não executada. ")     
 
